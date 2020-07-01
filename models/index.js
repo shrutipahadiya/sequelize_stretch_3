252073e098e5c7eb050624097bae422514b50932
Shruti Pahadiya <15.shruti@gmail.com>
2020-07-01 18:52:05 -0400

    stretch

--- models/index.js
+++ models/index.js
@@ -1,5 +1,17 @@
 const Person = require('./person');
 const Recipe = require('./recipe');
 //YOUR CODE HERE
+Person.hasMany(Receipe);
+Recipe.belongsTo(Person);
+
+Person.findAll({
+    include:{
+        model:Receipe,
+        where:{
+            
+        }
+    }
+
+})
 
 module.exports = { Person, Recipe };
